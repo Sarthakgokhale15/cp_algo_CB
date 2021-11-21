578c5f9dfb256ad4768d0e2d3a0310449c0f3535
sarthak Gokhale <sarthakgokhale15@gmail.com>
2021-11-21 20:48:37 +0530

    DAY4

--- DP/problems/LadderJump.cpp
+++ DP/problems/LadderJump.cpp
@@ -36,5 +36,22 @@ public:
 
         // top down approach(recursive)
         return solve(n, dp);
+
+        /*
+         *****bottom up space optimised
+        int a=1,b=1;
+        
+        int c=1;
+        for(int i=2;i<=n;i++){
+            
+               c=a+b; 
+            
+            
+            a=b;
+            b=c;
+        }
+        return c;
+        
+        */
     }
 };
